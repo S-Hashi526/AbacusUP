601a096d771a4a08b85502fb68ee71036d0a555d
s.hashi.0526@gmail.com <s.hashi.0526@gmail.com>
2023-10-16 22:40:21 +0900

    devise導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,17 @@
 Rails.application.routes.draw do
+  
   root 'static_pages#top'
-  get '/signup', to: 'users#new'
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'
+  }
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end
+
 end
