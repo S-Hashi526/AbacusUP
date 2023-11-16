f4dec1879af1d697c0e5da0f1439623f79b20ad3
s.hashi.0526@gmail.com <s.hashi.0526@gmail.com>
2023-11-17 06:23:52 +0900

    seedにて初期メン追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+
+User.create!(
+  [
+    {
+      name: "塾長",
+      email: "sample@email.com",
+      uid: "0001",
+      password: "password",
+      password_confirmation: "password",
+      principal: true,
+      instructor: true
+    },
+    {
+      name: "講師A",
+      email: "instractor-a@email.com",
+      uid: "1001",
+      password: "password",
+      password_confirmation: "password",
+      principal: false,
+      instructor: true
+    },
+    {
+      name: "講師B",
+      email: "instractor-b@email.com",
+      uid: "1002",
+      password: "password",
+      password_confirmation: "password",
+      principal: false,
+      instructor: true
+    }
+  ]
+)  
